c55e72adf403fa9b7832ba02de371799f38d8a97
Christian Thaeter <ct@pipapo.org>
2007-07-18 00:10:02 +0200

    example interface and C plugin

---
+++ tests/examples/hello_interface.h
@@ -0,0 +1,6 @@
+#include "plugin.h"
+
+CINELERRA_INTERFACE(hello, 1,
+                    CINELERRA_INTERFACE_PROTO(void, hello, (void)),
+                    CINELERRA_INTERFACE_PROTO(void, goodbye, (const char*)),
+                    );
