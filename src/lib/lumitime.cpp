3ae3e36f3f24f7323e48e3b26ee8d6aeb9f33c16
Ichthyostega <prg@ichthyostega.de>
2008-10-16 20:31:02 +0200

    @Joel: a simple convenience wrapper for gavl_time_t

--- src/lib/lumitime.cpp
+++ src/lib/lumitime.cpp
@@ -1,5 +1,5 @@
 /*
-  Time  -  unified representation of a time point, including conversion functions
+  Time  -  convenience wrapper for working with gavl_time in C++
  
   Copyright (C)         Lumiera.org
     2008,               Hermann Vosseler <Ichthyostega@web.de>
@@ -28,10 +28,9 @@
 namespace lumiera
   {
 
-   // TODO: dummy values; should be adjusted when switching to the real time implementation provided by the backend
   
-   const Time Time::MAX = +std::numeric_limits<long>::max();
-   const Time Time::MIN = -std::numeric_limits<long>::max();
+   const Time Time::MAX = +std::numeric_limits<int64_t>::max();
+   const Time Time::MIN = -std::numeric_limits<int64_t>::max();
 
 
 } // namespace lumiera
