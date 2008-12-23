121599dc897e7a31fca1ff2973c9a431c57567a2
Christian Thaeter <ct@pipapo.org>
2008-12-23 08:44:30 +0100

    lowest level thread api proposal

---
+++ src/backend/threads.c
@@ -0,0 +1,52 @@
+/*
+  threads.c  -  Manage threads
+
+  Copyright (C)         Lumiera.org
+    2008,               Christian Thaeter <ct@pipapo.org>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+//TODO: Support library includes//
+
+
+//TODO: Lumiera header includes//
+#include "backend/threads.h"
+
+//TODO: internal/static forward declarations//
+
+
+//TODO: System includes//
+
+
+/**
+ * @file
+ *
+ */
+
+NOBUG_DEFINE_FLAG_PARENT (threads, /*TODO insert parent flag here */);
+
+
+//code goes here//
+
+
+
+/*
+// Local Variables:
+// mode: C
+// c-file-style: "gnu"
+// indent-tabs-mode: nil
+// End:
+*/
