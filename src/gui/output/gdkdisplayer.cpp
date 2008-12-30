cdf26d3fe60ddaed487c14b543a582df624b68c5
Joel Holdsworth <joel@joel-desktop.(none)>
2008-12-30 13:06:08 +0000

    Tidied up the displayer code and added some documentation

--- src/gui/output/gdkdisplayer.cpp
+++ src/gui/output/gdkdisplayer.cpp
@@ -36,49 +36,49 @@ namespace output {
 
 GdkDisplayer::GdkDisplayer( Gtk::Widget *drawing_area, int width, int height ) :
     drawingArea( drawing_area )
-  {
-    REQUIRE(drawing_area != NULL);
-    REQUIRE(width > 0);
-    REQUIRE(height > 0);
-    
-    imageWidth = width, imageHeight = height;
-  }
+{
+  REQUIRE(drawing_area != NULL);
+  REQUIRE(width > 0);
+  REQUIRE(height > 0);
+  
+  imageWidth = width, imageHeight = height;
+}
 
 bool
 GdkDisplayer::usable()
-  {
-    return true;
-  }
+{
+  return true;
+}
 
 void
-GdkDisplayer::put( void *image )
-  {  
-    int video_x = 0, video_y = 0, video_width = 0, video_height = 0;
-    calculateVideoLayout(
-      drawingArea->get_width(),
-      drawingArea->get_height(),
-      preferredWidth(), preferredHeight(),
-      video_x, video_y, video_width, video_height );
+GdkDisplayer::put( const void* image )
+{  
+  int video_x = 0, video_y = 0, video_width = 0, video_height = 0;
+  calculateVideoLayout(
+    drawingArea->get_width(),
+    drawingArea->get_height(),
+    preferredWidth(), preferredHeight(),
+    video_x, video_y, video_width, video_height );
 
-    GdkWindow *window = drawingArea->get_window()->gobj();
-    REQUIRE(window != NULL);  
-      
-	  GdkGC *gc = gdk_gc_new( window );
-	  REQUIRE(gc != NULL);
-	  
-	  GdkPixbuf *pixbuf = gdk_pixbuf_new_from_data( (const guchar*)image, GDK_COLORSPACE_RGB, FALSE, 8,
-      preferredWidth(), preferredHeight(), preferredWidth() * 3, NULL, NULL );
-    REQUIRE(pixbuf != NULL);
-      
-	  GdkPixbuf *scaled_image = gdk_pixbuf_scale_simple( pixbuf, video_width, video_height, GDK_INTERP_NEAREST );
-	  REQUIRE(scaled_image != NULL);
-	  
-    gdk_draw_pixbuf( window, gc, scaled_image, 0, 0, video_x, video_y, -1, -1, GDK_RGB_DITHER_NORMAL, 0, 0 );
+  GdkWindow *window = drawingArea->get_window()->gobj();
+  REQUIRE(window != NULL);  
     
-	  g_object_unref( scaled_image );
-	  g_object_unref( pixbuf );
-	  g_object_unref( gc );
-  }
+  GdkGC *gc = gdk_gc_new( window );
+  REQUIRE(gc != NULL);
+  
+  GdkPixbuf *pixbuf = gdk_pixbuf_new_from_data( (const guchar*)image, GDK_COLORSPACE_RGB, FALSE, 8,
+    preferredWidth(), preferredHeight(), preferredWidth() * 3, NULL, NULL );
+  REQUIRE(pixbuf != NULL);
+    
+  GdkPixbuf *scaled_image = gdk_pixbuf_scale_simple( pixbuf, video_width, video_height, GDK_INTERP_NEAREST );
+  REQUIRE(scaled_image != NULL);
+  
+  gdk_draw_pixbuf( window, gc, scaled_image, 0, 0, video_x, video_y, -1, -1, GDK_RGB_DITHER_NORMAL, 0, 0 );
+  
+  g_object_unref( scaled_image );
+  g_object_unref( pixbuf );
+  g_object_unref( gc );
+}
 
 }   // namespace output
 }   // namespace gui
--- src/gui/output/xvdisplayer.hpp
+++ src/gui/output/xvdisplayer.hpp
@@ -46,30 +46,90 @@ namespace Gtk {
 namespace gui {
 namespace output {
 
-  class XvDisplayer : public Displayer
-  {
-  public:
-    XvDisplayer( Gtk::Widget *drawing_area, int width, int height );
-    ~XvDisplayer();
-
-    void put( void *image );
+/**
+ * XvDisplayer is a class which is responsible for rendering  a video
+ * image via XVideo.
+ **/
+class XvDisplayer : public Displayer
+{
+public:
+  /**
+   * Constructor
+   * @param drawing_area The widget into which the video image will be
+   * drawn. This value must not be NULL.
+   * @param width The width of the video image in pixels. This value
+   * must be greater than zero.
+   * @param height The height of the video image in pixels. This value
+   * must be greater than zero.
+   **/
+  XvDisplayer( Gtk::Widget *drawing_area, int width, int height );
   
-  protected:
-    bool usable();
+  /**
+   * Destructor
+   **/
+  ~XvDisplayer();
 
-  private:
-	  bool gotPort;
-	  int grabbedPort;
-	  Gtk::Widget *drawingArea;
-	  Display *display;
-	  Window window;
-	  GC gc;
-	  XGCValues values;
-	  XvImage *xvImage;
-	  unsigned int port;
-	  XShmSegmentInfo shmInfo;
-	  char pix[ MAX_WIDTH * MAX_HEIGHT * 4 ];
-  };
+  /**
+   * Put an image of a given width and height with the expected input
+   * format (as indicated by the format method).
+   * @param[in] image The video image array to draw.
+   */
+  void put( const void* image );
+
+  /** 
+   * Indicates if this object can be used to render images on the
+   * running system.
+   */
+  bool usable();
+
+private:
+
+  /**
+   * Specifies whether the object is currently attached to an XVideo
+   * port.
+   * @remarks This value is false until the constructor has finished
+   * successfully.
+   **/
+  bool gotPort;
+  
+  /**
+   * The current port being used.
+   * @remarks This value is meaninless unless gotPort is true.
+   **/
+  unsigned int grabbedPort;
+  
+  /**
+   * The widget that video will be drawn into.
+   * @remarks This value must be a valid pointer.
+   **/
+  Gtk::Widget *drawingArea;
+  
+  /**
+   * The display that video will be drawn into.
+   **/
+  Display *display;
+  
+  /**
+   * The X11 window that video will be drawn into.
+   **/
+  Window window;
+  
+  /**
+   * The graphics context which will be used when rednering video.
+   **/
+  GC gc;
+  
+  /**
+   * The shared memory image object which video will be written into.
+   **/
+  XvImage *xvImage;
+  
+  /**
+   * Info about the shared memory segment.
+   * @remarks shmInfo.shmaddr is set to NULL, when the SHM is detached.
+   **/
+  XShmSegmentInfo shmInfo;
+};
 
 }   // namespace output
 }   // namespace gui
