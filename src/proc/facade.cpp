608c532df744e9b3d90d55f03b65198de5784e62
Ichthyostega <prg@ichthyostega.de>
2009-06-02 03:32:48 +0200

    Fix: wrong wiring of subsystem descriptor for the Session

--- src/proc/facade.cpp
+++ src/proc/facade.cpp
@@ -105,7 +105,7 @@ namespace proc {
   
   namespace {
     lumiera::Singleton<BuilderSubsysDescriptor> theBuilderDescriptor;   
-    lumiera::Singleton<BuilderSubsysDescriptor> theSessionDescriptor;
+    lumiera::Singleton<SessionSubsysDescriptor> theSessionDescriptor;
   }
   
   
