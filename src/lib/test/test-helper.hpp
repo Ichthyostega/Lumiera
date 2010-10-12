b15a1c2d3ce61d13dc38c0d026cd0f82613ed967
Ichthyostega <prg@ichthyostega.de>
2010-10-12 02:38:18 +0200

    changed wording of the error message

--- src/lib/test/test-helper.hpp
+++ src/lib/test/test-helper.hpp
@@ -129,15 +129,16 @@ namespace test{
  * an assertion failure. In case of an exception, the #lumiera_error
  * state is checked, cleared and verified.
  */
-#define VERIFY_ERROR(ERROR_ID, ERRONEOUS_STATEMENT)                             \
-          try                                                                   \
-            {                                                                   \
-              ERRONEOUS_STATEMENT ;                                             \
-              NOTREACHED("'%s' resisted to fail", #ERRONEOUS_STATEMENT);        \
-            }                                                                   \
-          catch (...)                                                           \
-            {                                                                   \
-              CHECK (lumiera_error_expect (LUMIERA_ERROR_##ERROR_ID));          \
+#define VERIFY_ERROR(ERROR_ID, ERRONEOUS_STATEMENT)          \
+          try                                                 \
+            {                                                  \
+              ERRONEOUS_STATEMENT ;                             \
+              NOTREACHED("expected '%s' failure in: %s",         \
+                          #ERROR_ID, #ERRONEOUS_STATEMENT);       \
+            }                                                      \
+          catch (...)                                               \
+            {                                                        \
+              CHECK (lumiera_error_expect (LUMIERA_ERROR_##ERROR_ID));\
             }
 
 #endif
