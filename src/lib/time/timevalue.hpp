f18226e8ac3f6aa3272187ae8cb7242b61159ed7
Ichthyostega <prg@ichthyostega.de>
2010-12-22 04:49:57 +0100

    WIP start drafting simple time values, offsets and intervals
    questions: what is mutable / immutable?
    what is the distinction between TimeValue and Time ?
    what conversions make sense?
    what mutations / operators to support?

--- src/lib/time/timevalue.hpp
+++ src/lib/time/timevalue.hpp
@@ -24,8 +24,13 @@
 #ifndef LIB_TIME_TIMEVALUE_H
 #define LIB_TIME_TIMEVALUE_H
 
-//#include <boost/operators.hpp>
+#include <boost/operators.hpp>
 #include <string>
+
+extern "C" {
+#include <stdint.h>
+#include <gavl/gavltime.h>
+}
 
 
 namespace lib {
@@ -38,11 +43,117 @@ namespace time {
    * @todo WIP-WIP-WIP
    */
   class TimeValue
+    : boost::totally_ordered<Time,
+      boost::totally_ordered<Time, gavl_time_t> >
     {
+      gavl_time_t t_;
       
     public:
+      static const Time MAX ; 
+      static const Time MIN ;
+      
+      explicit 
+      TimeValue (gavl_time_t val=0)
+        : t_(val)
+        { }
+      
+      // using standard copy operations
+      
+      operator gavl_time_t ()  const { return t_; }
+      
+      // Supporting totally_ordered
+      friend bool operator<  (Time const& t1, Time const& t2)  { return t1.t_ <  t2.t_; }
+      friend bool operator<  (Time const& t1, gavl_time_t t2)  { return t1.t_ <  t2   ; }
+      friend bool operator>  (Time const& t1, gavl_time_t t2)  { return t1.t_ >  t2   ; }
+      friend bool operator== (Time const& t1, Time const& t2)  { return t1.t_ == t2.t_; }
+      friend bool operator== (Time const& t1, gavl_time_t t2)  { return t1.t_ == t2   ; }
     };
   
+  
+  class TimeVar
+    : public TimeValue
+    , boost::additive<TimeVar>
+    {
+        
+    public:
+      TimeVar (TimeValue time)
+        : TimeValue(time)
+        { }
+      
+      
+      // Supporting additive
+      TimeVar& operator+= (TimeVar const& tx)  { t_ += tx.t_; return *this; }
+      TimeVar& operator-= (TimeVar const& tx)  { t_ -= tx.t_; return *this; }
+      
+      // Supporting multiplication with integral factor
+      TimeVar& operator*= (int64_t fact)       { t_ *= fact;  return *this; }
+       
+      // baseclass TimeValue is already totally_ordered 
+    };
+  
+  
+  class Offset;  
+    
+  /**
+   * Lumiera's internal time value datatype
+   */
+  class Time
+    : public TimeValue
+    {
+    public:
+      explicit 
+      Time (TimeValue val=0)
+        : TimeValue(val)
+        { }
+      
+      Time ( long millis
+           , uint secs 
+           , uint mins =0
+           , uint hours=0
+           );
+    };
+  
+  
+  class Offset
+    : public TimeValue
+    {
+        
+    public:
+      explicit 
+      Offset (TimeValue distance)
+        : TimeValue(distance)
+        { }
+    };
+      
+  inline Offset
+  operator- (Time const& end, Time const& start)
+  {
+    TimeVar distance(end);
+    distance -= start;
+    return Offset(distance);
+  }
+    
+  typedef const Offset TimeDistance;
+  
+  
+  class Duration
+    : public TimeValue
+    {
+      // always positive
+    };
+  
+  
+  class TimeSpan
+    : public Time
+    {
+      Duration dur_;
+      
+    public:
+      TimeSpan(Time start, Duration length)
+        : Time(start)
+        , dur_(length)
+        { }
+    };
   
   
 }} // lib::time
