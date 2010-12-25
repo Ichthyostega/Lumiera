063e5887b4e3360ae0f7e16ef10b90e2a690ad97
Stefan Kangas <stefankangas@gmail.com>
2010-12-25 04:39:30 +0100

    Fix Ardour Timecode widget: compilation problems on the buildserver. (Ticket #735)

--- src/gui/widgets/timecode-widget.cpp
+++ src/gui/widgets/timecode-widget.cpp
@@ -187,7 +187,8 @@ TimeCode::set_widget_name(string name)
 void
 TimeCode::setup_events()
 {
-  clock_base.set_can_focus(true);
+  // FIXME: change to set_can_focus(true) once Debian squeeze is released
+  clock_base.set_flags(CAN_FOCUS);
 
   const Gdk::EventMask eventMask =
     Gdk::BUTTON_PRESS_MASK|
@@ -207,14 +208,15 @@ TimeCode::setup_events()
   ms_seconds_ebox.add_events(eventMask);
   audio_frames_ebox.add_events(eventMask);
 
-  hours_ebox.set_can_focus(true);
-  minutes_ebox.set_can_focus(true);
-  seconds_ebox.set_can_focus(true);
-  frames_ebox.set_can_focus(true);
-  audio_frames_ebox.set_can_focus(true);
-  ms_hours_ebox.set_can_focus(true);
-  ms_minutes_ebox.set_can_focus(true);
-  ms_seconds_ebox.set_can_focus(true);
+  // FIXME: change to set_can_focus(true) once Debian squeeze is released
+  hours_ebox.set_flags(CAN_FOCUS);
+  minutes_ebox.set_flags(CAN_FOCUS);
+  seconds_ebox.set_flags(CAN_FOCUS);
+  frames_ebox.set_flags(CAN_FOCUS);
+  audio_frames_ebox.set_flags(CAN_FOCUS);
+  ms_hours_ebox.set_flags(CAN_FOCUS);
+  ms_minutes_ebox.set_flags(CAN_FOCUS);
+  ms_seconds_ebox.set_flags(CAN_FOCUS);
 
   hours_ebox.signal_motion_notify_event().connect(bind(mem_fun(
     *this, &TimeCode::field_motion_notify_event), SMPTE_Hours));
