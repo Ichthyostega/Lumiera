4ece1352572902de34e548bf3f3022a60583c60e
Ichthyostega <prg@ichthyostega.de>
2011-06-18 03:34:27 +0200

    cont.. drafting OutputSlot

--- src/proc/play/output-slot.cpp
+++ src/proc/play/output-slot.cpp
@@ -1,5 +1,5 @@
 /*
-  OUTPUT-MANAGER.hpp  -  handling all the real external output connections
+  OutputSlot  -  capability to transfer data to a physical output
 
   Copyright (C)         Lumiera.org
     2011,               Hermann Vosseler <Ichthyostega@web.de>
@@ -18,55 +18,31 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-*/
-
-/** @file output-manager.hpp
- ** A global service to handle all external output connections.
- **
- ** @see output-manager-test.cpp  ////TODO
- */
+* *****************************************************/
 
 
-#ifndef PROC_PLAY_OUTPUT_MANAGER_H
-#define PROC_PLAY_OUTPUT_MANAGER_H
-
-
-#include "lib/error.hpp"
 #include "proc/play/output-slot.hpp"
 
-#include <boost/noncopyable.hpp>
-//#include <string>
-//#include <vector>
-#include <tr1/memory>
 
 
 namespace proc {
 namespace play {
   
-//using std::string;
-//using std::vector;
-  using std::tr1::shared_ptr;
+  
+  
+  namespace { // hidden local details of the service implementation....
+    
+  } // (End) hidden service impl details
   
   
   
+  OutputSlot::~OutputSlot() { }  // emit VTables here....
   
   
   
+  /**  */
   
-  /******************************************************
-   * Management of external Output connections.
-   * 
-   * @todo write Type comment
-   */
-  class OutputManager
-    : boost::noncopyable
-    {
-    public:
-      OutputManager() {}
-    };
   
-  typedef shared_ptr<OutputManager> POutputManager;
   
   
 }} // namespace proc::play
-#endif
---
+++ src/proc/play/output-slot.hpp
@@ -0,0 +1,101 @@
+/*
+  OUTPUT-SLOT.hpp  -  capability to transfer data to a physical output
+
+  Copyright (C)         Lumiera.org
+    2011,               Hermann Vosseler <Ichthyostega@web.de>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+/** @file output-slot.hpp
+ ** An (abstract) capability to send media data to an external output.
+ ** OutputSlot is the central metaphor for the organisation of actual (system level) outputs;
+ ** using this concept allows to separate and abstract the data calculation and the organisation
+ ** of playback and rendering from the specifics of the actual output sink. Actual output
+ ** possibilities can be added and removed dynamically from various components (backend, GUI),
+ ** all using the same resolution and mapping mechanisms 
+ **
+ ** @see output-test-slot.hpp  ////TODO
+ */
+
+
+#ifndef PROC_PLAY_OUTPUT_SLOT_H
+#define PROC_PLAY_OUTPUT_SLOT_H
+
+
+#include "lib/error.hpp"
+#include "lib/handle.hpp"
+#include "lib/time/timevalue.hpp"
+//#include "lib/sync.hpp"
+
+#include <boost/noncopyable.hpp>
+//#include <string>
+//#include <vector>
+//#include <tr1/memory>
+//#include <boost/scoped_ptr.hpp>
+
+
+namespace proc {
+namespace play {
+  
+//using std::string;
+//using std::vector;
+//using std::tr1::shared_ptr;
+//using boost::scoped_ptr;
+  
+  
+  
+  class Connection;
+  
+  class BufferHandoverSink
+    : public lib::Handle<Connection>
+    {
+      
+    public:
+      void emit(Time);
+    };
+  
+  
+  class SharedBufferSink
+    : public lib::Handle<Connection>
+    {
+      
+    public:
+      void emit(Time);
+    };
+  
+  
+  
+  
+  /********************************************************************
+   * Interface: Generic output sink.
+   * 
+   * @todo write type comment
+   */
+  class OutputSlot
+    : boost::noncopyable
+    {
+    public:
+      virtual ~OutputSlot();
+      
+    private:
+      
+    };
+  
+  
+  
+}} // namespace proc::play
+#endif
