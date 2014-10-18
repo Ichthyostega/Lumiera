ac5816df4f459c4fd273245205f3829bb148e8cf
Ichthyostega <prg@ichthyostega.de>
2014-10-18 21:39:13 +0200

    DOC: expand on the core concepts of workflow

--- wiki/workflow.mm
+++ wiki/workflow.mm
@@ -1,29 +1,138 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1413510585979" ID="ID_1838403826" MODIFIED="1413510614519" TEXT="Workflow">
-<font NAME="SansSerif" SIZE="18"/>
-<node CREATED="1413510717980" ID="ID_1036140869" MODIFIED="1413510723715" POSITION="right" TEXT="things at hand">
-<node CREATED="1413510868815" ID="ID_1206980305" MODIFIED="1413510872755" TEXT="focus">
-<node CREATED="1413510908200" ID="ID_596718260" MODIFIED="1413510911515" TEXT="scope">
-<node CREATED="1413510991455" ID="ID_562651196" MODIFIED="1413510993307" TEXT="nesting">
+<node CREATED="1413510585979" ID="ID_1838403826" MODIFIED="1413659193954" TEXT="Workflow">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1413510717980" HGAP="23" ID="ID_1036140869" MODIFIED="1413659200994" POSITION="right" TEXT="things at hand" VGAP="2" VSHIFT="66">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1413657433263" ID="ID_1710913666" MODIFIED="1413660998737" VSHIFT="93">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        start with simple stuff
+      </li>
+      <li>
+        do what we can do now
+      </li>
+      <li>
+        prepare for the future to emerge
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1413510868815" HGAP="174" ID="ID_1206980305" MODIFIED="1413661005753" TEXT="focus" VSHIFT="2">
+<node CREATED="1413510908200" ID="ID_596718260" MODIFIED="1413659231265" TEXT="scope">
+<node CREATED="1413510991455" ID="ID_562651196" MODIFIED="1413659245953" TEXT="nesting">
 <node CREATED="1413511033231" ID="ID_742035972" MODIFIED="1413511042243" TEXT="find applicable settings"/>
 <node CREATED="1413511054655" ID="ID_159258987" MODIFIED="1413511058140" TEXT="nesting on demand"/>
 </node>
 <node CREATED="1413510993879" ID="ID_174295150" MODIFIED="1413511000131" TEXT="expand and collapse"/>
 </node>
-<node CREATED="1413510912911" ID="ID_474240961" MODIFIED="1413510922987" TEXT="select to use"/>
+<node CREATED="1413510912911" HGAP="114" ID="ID_474240961" MODIFIED="1413660832601" TEXT="select to use" VSHIFT="1">
+<node CREATED="1413659417377" ID="ID_936891230" MODIFIED="1413659422029" TEXT="things to operate on"/>
+<node CREATED="1413659425505" ID="ID_1509372621" MODIFIED="1413659446765" TEXT="things to receive"/>
+<node CREATED="1413659470001" ID="ID_1411256387" MODIFIED="1413659474085" TEXT="group selection"/>
+<node CREATED="1413660033442" ID="ID_1070918387" MODIFIED="1413660037590" TEXT="range selection">
+<node CREATED="1413660249850" ID="ID_1639354177" MODIFIED="1413660286872">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      heuristics to
+    </p>
+    <p>
+      <i>transform</i>&#160;into a group selection
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
 <node CREATED="1413510941701" ID="ID_1142892871" MODIFIED="1413510945235" TEXT="work site"/>
 </node>
+<node CREATED="1413655274303" HGAP="273" ID="ID_1069937326" MODIFIED="1413659221573" TEXT="work sites" VSHIFT="22">
+<node CREATED="1413657122975" ID="ID_1471903102" MODIFIED="1413659245958" TEXT="emerging worksites"/>
+<node CREATED="1413657130015" ID="ID_751226673" MODIFIED="1413659245967" TEXT="preconfigured standard worksites"/>
+<node CREATED="1413658012504" ID="ID_44452627" MODIFIED="1413659245968">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      you move <i>into</i>&#160;a worksite
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
-<node CREATED="1413510819479" ID="ID_990501270" MODIFIED="1413510821788" POSITION="right" TEXT="adapt"/>
-<node CREATED="1413510737455" ID="ID_783538322" MODIFIED="1413510744939" POSITION="left" TEXT="usage scenario">
-<node CREATED="1413511067895" ID="ID_817682123" MODIFIED="1413511168083" TEXT="exploring media"/>
-<node CREATED="1413511168863" ID="ID_900928608" MODIFIED="1413511177635" TEXT="simple assembly"/>
+</node>
+<node CREATED="1413656153950" HGAP="-9" ID="ID_1892669129" MODIFIED="1413660705224" TEXT="control binding" VSHIFT="-12">
+<node CREATED="1413656881759" ID="ID_1317014198" MODIFIED="1413659242017" TEXT="global focus moving gestures">
+<node CREATED="1413657792712" ID="ID_1842036488" MODIFIED="1413657804020" TEXT="very few, obvious"/>
+<node CREATED="1413657804824" ID="ID_1427699026" MODIFIED="1413657815772" TEXT="spatially oriented"/>
+</node>
+<node CREATED="1413656901383" ID="ID_829873757" MODIFIED="1413659245975" TEXT="bind to focus"/>
+</node>
+</node>
+<node CREATED="1413656215134" HGAP="-177" ID="ID_58098372" MODIFIED="1413661016553" POSITION="right" TEXT="access" VSHIFT="-86">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1413656395470" ID="ID_1750084836" MODIFIED="1413659245986">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      few things
+    </p>
+    <p>
+      yet deeply nested
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1413656279534" ID="ID_372854270" MODIFIED="1413656370243">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      no fuss for the beginners
+    </p>
+    <p>
+      optimise for the proficient
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1413510737455" HGAP="31" ID="ID_783538322" MODIFIED="1413660796033" POSITION="left" TEXT="usage scenario" VSHIFT="57">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1413511067895" ID="ID_817682123" MODIFIED="1413659245990" TEXT="exploring media"/>
+<node CREATED="1413511168863" ID="ID_900928608" MODIFIED="1413659245991" TEXT="simple assembly"/>
 <node CREATED="1413511186751" ID="ID_1038494350" MODIFIED="1413511198675" TEXT="augmenting"/>
 <node CREATED="1413511203479" ID="ID_180571783" MODIFIED="1413511207131" TEXT="compositional work"/>
 <node CREATED="1413511214832" ID="ID_5174859" MODIFIED="1413511218650" TEXT="sound integration"/>
-<node CREATED="1413511225472" ID="ID_849942074" MODIFIED="1413511228171" TEXT="large projects"/>
+<node CREATED="1413511225472" ID="ID_849942074" MODIFIED="1413661071987" TEXT="large projects"/>
 <node CREATED="1413511229375" ID="ID_571114920" MODIFIED="1413511231443" TEXT="teamwork"/>
+</node>
+<node CREATED="1413510819479" HGAP="81" ID="ID_990501270" MODIFIED="1413660798713" POSITION="left" TEXT="adapt" VGAP="2" VSHIFT="76">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1413655040934" ID="ID_1167242478" MODIFIED="1413659145231" TEXT="to necessary tooling"/>
+<node CREATED="1413655051189" ID="ID_1073267383" MODIFIED="1413655055009" TEXT="to scope of project"/>
+<node CREATED="1413655510493" ID="ID_1262130680" MODIFIED="1413655518009" TEXT="to working style"/>
+<node CREATED="1413655037401" ID="ID_134783492" MODIFIED="1413655039961" TEXT="to habits"/>
 </node>
 </node>
 </map>
