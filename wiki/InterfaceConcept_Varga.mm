05e5ab95a3881603654d07e5fe96d5e939953180
Christoph Varga <christoph.varga@werk-wien.at>
2015-01-24 10:00:00 +0100

    Lumiera GUI thoughts -- early draft for an interface concept
    note by the committer: this mindmap was really work in progress.
    Christoph shared it with me while in discussion. I'll place it
    here in the git history, since it might be interesting to se
    how the thoughts evolved. Isn't that what mindmaps are all about?

---
+++ wiki/InterfaceConcept_Varga.mm
@@ -0,0 +1,190 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Operating System" ID="ID_1631706542" CREATED="1421688431835" MODIFIED="1421875739706" VGAP="1">
+<edge STYLE="sharp_bezier" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12"/>
+<hook NAME="MapStyle" zoom="0.75">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Workboards" POSITION="left" ID="ID_957022120" CREATED="1421689856157" MODIFIED="1421875416102" HGAP="12" VSHIFT="796">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_957022120" ENDARROW="None" ENDINCLINATION="-471;88;" ID="Arrow_ID_1910682021" SOURCE="ID_21270584" STARTARROW="Default" STARTINCLINATION="-215;12;"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#00cc99" WIDTH="2" TRANSPARENCY="120" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1035997003" STARTINCLINATION="61;5;" ENDINCLINATION="-85;-119;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="2D space where you can place all data which is dedicated to one project for exchange between apps and for organizing them. &#xa;Things like files, notes, link to disks, link to all the apps you need, .. And also there should be a clipboard where files can be stored temporarily for exchange between different apps.&#xa;Depending on what file the user is clicking it leads directly to one of the &apos;Stages&apos; within Lumiera." ID="ID_903148200" CREATED="1421690272218" MODIFIED="1421691008728">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node TEXT="Applications" POSITION="left" ID="ID_308197395" CREATED="1421689994974" MODIFIED="1421869020366" HGAP="13" VSHIFT="-728">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node TEXT="Lumiera NLE" ID="ID_799374750" CREATED="1421688581377" MODIFIED="1421875755612" HGAP="28" VSHIFT="96">
+<edge STYLE="sharp_bezier" COLOR="#6699ff" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="12" BOLD="true"/>
+<node TEXT="Playheads" ID="ID_663083724" CREATED="1421869841620" MODIFIED="1421877302687" HGAP="230" VSHIFT="-500">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" DASH="3 3" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_64799033" STARTINCLINATION="-263;28;" ENDINCLINATION="-154;-11;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" DASH="3 3" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_440912401" STARTINCLINATION="-304;37;" ENDINCLINATION="-92;22;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" DASH="3 3" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1789129602" STARTINCLINATION="-320;41;" ENDINCLINATION="-78;-36;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" DASH="3 3" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1193480024" MIDDLE_LABEL="permanently connected" STARTINCLINATION="103;22;" ENDINCLINATION="-52;-53;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+<hook NAME="FreeNode"/>
+<node TEXT="This is for time based views only. Each viewer have one playhead. Playheads are static in the middle of the viewer and the media is transported through. (so we don&apos;t have the situation that the playhead is moving through the window and the timeline have to jump afterwards.)" ID="ID_1585966136" CREATED="1421869857851" MODIFIED="1421871422903"/>
+</node>
+<node TEXT="Permanent Area" ID="ID_1193480024" CREATED="1421868859081" MODIFIED="1421875345053" HGAP="270" VSHIFT="-80">
+<font SIZE="14"/>
+<arrowlink SHAPE="LINE" COLOR="#ccccff" WIDTH="30" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1035997003" STARTINCLINATION="293;0;" ENDINCLINATION="293;0;" STARTARROW="NONE" ENDARROW="NONE"/>
+<node TEXT="Viewer A" ID="ID_103452312" CREATED="1421868937382" MODIFIED="1421869091810">
+<font BOLD="true"/>
+<node TEXT="Viewer A is a part of the screen in flexible size which shows all active media." ID="ID_1028303235" CREATED="1421869132696" MODIFIED="1421869202803"/>
+</node>
+<node TEXT="Viewer B" ID="ID_1366908315" CREATED="1421868955142" MODIFIED="1421868959072">
+<node TEXT="Viewer B is a second deck which is situated left from viewer A. The user can swich between A and B or show them in differen proportion. You can use one as in other NLE&apos;s to view the source media and the other for a sequence or have two parts of the same sequence open for comparison or easy coping clips around.&#xa;there is always one deck/ viewer active so that our navigation input (e.g. via shuttle) always adresses one." ID="ID_1788168808" CREATED="1421869204062" MODIFIED="1421871512164"/>
+</node>
+</node>
+<node TEXT="Stages (Working Environments)" ID="ID_1035997003" CREATED="1421688837579" MODIFIED="1421877297639" HGAP="247" VSHIFT="5">
+<edge STYLE="sharp_bezier" COLOR="#3399ff" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="14"/>
+<linktarget COLOR="#00cc99" DESTINATION="ID_1035997003" ENDARROW="Default" ENDINCLINATION="-7;-146;" ID="Arrow_ID_390886322" SOURCE="ID_957022120" STARTARROW="None" STARTINCLINATION="52;-65;"/>
+<arrowlink SHAPE="LINE" COLOR="#ccccff" WIDTH="14" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1335337913" STARTINCLINATION="257;0;" ENDINCLINATION="257;0;" STARTARROW="NONE" ENDARROW="NONE"/>
+<node TEXT="Media Manager (source)" ID="ID_1350811385" CREATED="1421688933520" MODIFIED="1421877281459">
+<edge STYLE="sharp_bezier" COLOR="#3399ff" WIDTH="8"/>
+</node>
+<node TEXT="Project Organizer" ID="ID_1355866027" CREATED="1421688988255" MODIFIED="1421877281463">
+<edge STYLE="sharp_bezier" COLOR="#3399ff" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="12" BOLD="true"/>
+<node TEXT="automatic or manual group objects with a few different render options for the groups/bins. bins are universal objects which can transform to finished timelines and can be viewed in all the &apos;Views&apos; provided in the &apos;Editor&apos; stage. &apos;Views&apos; are sub stages.&#xa;In &apos;Project Organizer&apos; you have to define settings for each project such as how many sub-stages you will see, which hardware interfaces, how detailed options you want to see when exporting,... there should be some pre-configured scenarios five or so which covers all your needs. one of them should be &apos;easy setup&apos; for beginners, online production,... and pro users can make their own.&#xa;this settings can be easily modified during the editing process but the aim is to have as less things around as possible.&#xa;maybe it&apos;s good to select a user here so that every project can include one ore more users and they can have their settings (environment)." ID="ID_1309625965" CREATED="1421693452494" MODIFIED="1421839550210"/>
+</node>
+<node TEXT="Editor" ID="ID_935033089" CREATED="1421689424077" MODIFIED="1421877281464">
+<edge STYLE="sharp_bezier" COLOR="#3399ff" WIDTH="8"/>
+<node TEXT="Views: Video A" ID="ID_813068038" CREATED="1421693200606" MODIFIED="1421834900949">
+<node TEXT="magazine (newspaper) view, where you can explore your shooting. with description and tagging clips inside this bin/sequence/group" ID="ID_1478131109" CREATED="1421693353769" MODIFIED="1421877204549">
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node TEXT="all the views should be available in a mouse/touch and keyboard/&apos;lumiera hardware interface&apos; only version. so there are different environments without compromises" ID="ID_1883657315" CREATED="1421834595396" MODIFIED="1421834825875">
+<edge COLOR="#ff9999"/>
+<font NAME="SansSerif" SIZE="12" ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Video B" ID="ID_35799295" CREATED="1421693288323" MODIFIED="1421693292925">
+<node TEXT="Loose arrangement and clip connecting in 2D space" ID="ID_251653884" CREATED="1421833724647" MODIFIED="1421877204560">
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="Video C" ID="ID_1712184626" CREATED="1421693293897" MODIFIED="1421693296581">
+<node TEXT="define time as a flexible line object" ID="ID_1172832176" CREATED="1421833811820" MODIFIED="1421877204561">
+<edge STYLE="bezier" WIDTH="thin"/>
+<node TEXT="&apos;Relation Spyder&apos; tool which sets the relations throught a static wall of clips, this produces a fexible line which is/become a new object (sequence)&#xa;sketch:" ID="ID_1990922797" CREATED="1421876866923" MODIFIED="1421877341179" HGAP="698" VSHIFT="-40">
+<edge STYLE="linear" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node TEXT="Video D" ID="ID_64799033" CREATED="1421693297131" MODIFIED="1421693300261">
+<node TEXT="arrange clips according to the &apos;time ray&apos; or line, so now the time goes continually forward " ID="ID_1791626974" CREATED="1421833865857" MODIFIED="1421877204561">
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="Video E" ID="ID_440912401" CREATED="1421693300930" MODIFIED="1421693303821">
+<node TEXT="precision trim in a track based linear manner (very classical)" ID="ID_1081334440" CREATED="1421834029165" MODIFIED="1421877204562">
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="Video F" ID="ID_170073817" CREATED="1421693304747" MODIFIED="1421693312381">
+<node TEXT="flow chart, nodes, meta organizer" ID="ID_1897507803" CREATED="1421836743045" MODIFIED="1421877204562">
+<edge STYLE="bezier" WIDTH="thin"/>
+<node TEXT="its possible to create relations between objects e.g. Master-Slave for undertitle positions, effects, clip X on a specific point in time is linked to clip Y on a specific point in time, clip Z is linked to a &apos;track&apos; object on time 0,0 (which is a normal track based timline);&#xa;..........." ID="ID_724972898" CREATED="1421877384276" MODIFIED="1421877826937"/>
+</node>
+</node>
+<node TEXT="Video G" ID="ID_1504447149" CREATED="1421693316538" MODIFIED="1421836850230">
+<node TEXT="storyboard (stills)" ID="ID_570194982" CREATED="1421837217853" MODIFIED="1421877204563">
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="Pr&#xe4;sentations Modus" ID="ID_1954213973" CREATED="1422010227433" MODIFIED="1422010249668"/>
+<node TEXT="Effects" ID="ID_1784867392" CREATED="1421836863882" MODIFIED="1421836941934"/>
+<node TEXT="Audio" ID="ID_1789129602" CREATED="1421693239244" MODIFIED="1421693242942"/>
+<node TEXT="Text tool" ID="ID_1019385943" CREATED="1421836883496" MODIFIED="1421836888491"/>
+<node TEXT="Color grading" ID="ID_1384014116" CREATED="1421693243796" MODIFIED="1421693248326"/>
+<node TEXT="(VFX external)" ID="ID_994646344" CREATED="1421693248963" MODIFIED="1421870345004">
+<edge COLOR="#009999"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" DASH="2 7" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1382218680" STARTINCLINATION="-282;0;" ENDINCLINATION="89;0;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="(Audio external)" ID="ID_139552117" CREATED="1421833310388" MODIFIED="1421870359669">
+<edge COLOR="#009999"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" DASH="2 7" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_981858212" STARTINCLINATION="-139;17;" ENDINCLINATION="49;-109;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="(Color grading external)" ID="ID_1170018192" CREATED="1421833355530" MODIFIED="1421870369323">
+<edge COLOR="#009999"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" DASH="2 7" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_21270584" STARTINCLINATION="-100;-115;" ENDINCLINATION="63;144;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+</node>
+</node>
+<node TEXT="Export/Delivery" ID="ID_1335679422" CREATED="1421691750314" MODIFIED="1421877281480">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+</node>
+</node>
+<node TEXT="(meta) info layer" ID="ID_1335337913" CREATED="1421691951616" MODIFIED="1421870725544" HGAP="224" VSHIFT="44">
+<font SIZE="14"/>
+<node TEXT="this layer puts detailed information over everything actual on the screen and should act as a quick lookup. there can also be git-tickets on the elements for easy user-feedback." ID="ID_693210573" CREATED="1421869573267" MODIFIED="1421869748986"/>
+</node>
+<node TEXT="settings layer" ID="ID_1861052741" CREATED="1421691997536" MODIFIED="1421870730643" HGAP="224" VSHIFT="-69">
+<font SIZE="14"/>
+<node TEXT="settings as a layer should show only settings which are relevant to what we see now." ID="ID_740976869" CREATED="1421869758888" MODIFIED="1421869804136"/>
+</node>
+</node>
+<node TEXT="Color grading tool" ID="ID_21270584" CREATED="1421833449694" MODIFIED="1421875791663">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_21270584" ENDARROW="Default" ENDINCLINATION="63;144;" ID="Arrow_ID_299541508" SOURCE="ID_1170018192" STARTARROW="Default" STARTINCLINATION="-100;-115;"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" DASH="2 7" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_957022120" STARTINCLINATION="-215;12;" ENDINCLINATION="-471;88;" STARTARROW="NONE" ENDARROW="NONE"/>
+</node>
+<node TEXT="Audio tool" ID="ID_981858212" CREATED="1421833473622" MODIFIED="1421833665013">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_981858212" ENDARROW="Default" ENDINCLINATION="49;-109;" ID="Arrow_ID_1916075795" SOURCE="ID_139552117" STARTARROW="Default" STARTINCLINATION="-139;17;"/>
+</node>
+<node TEXT="VFX tool" ID="ID_1382218680" CREATED="1421833506781" MODIFIED="1421833631238">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1382218680" ENDARROW="Default" ENDINCLINATION="89;0;" ID="Arrow_ID_1748651927" SOURCE="ID_994646344" STARTARROW="Default" STARTINCLINATION="-282;0;"/>
+</node>
+</node>
+</node>
+</map>
