c70ad01ad3a842d410a87f43da3568324e4710a9
Ichthyostega <prg@ichthyostega.de>
2015-06-12 23:23:23 +0200

    resolve some conflicts in the design of the "External Tree Representation"
    for the purpose of working out the inner logic, I frequently use the
    help of a mindmap -- so why not commiting this alongside? For sure,
    it is preliminary and the worked out concepts will be cast in code
    and documented on the website. Yet the thought-process leading to
    these decisions might be of some interest, at least for myself.

---
+++ wiki/thinkPad.ichthyo.mm
@@ -0,0 +1,239 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#6666ff" CREATED="1434127882200" ID="ID_1452170048" MODIFIED="1434128038348" TEXT="Lumi">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1434128046296" ID="ID_1900827283" MODIFIED="1434128053553" POSITION="right" TEXT="GUI">
+<node CREATED="1434128054470" ID="ID_1166611516" MODIFIED="1434128059666" TEXT="Workflow"/>
+<node CREATED="1434128059966" ID="ID_823283341" MODIFIED="1434128067529" TEXT="Connect">
+<node CREATED="1434128071126" ID="ID_1618124128" MODIFIED="1434128074137" TEXT="UI-Bus">
+<node CREATED="1434128297445" ID="ID_1971555917" MODIFIED="1434128300889" TEXT="Nachrichtenformat"/>
+<node CREATED="1434128301525" ID="ID_187622243" MODIFIED="1434128303993" TEXT="Parallelit&#xe4;t"/>
+<node CREATED="1434128332277" ID="ID_33025591" MODIFIED="1434128337777" TEXT="Deregistrierung"/>
+<node CREATED="1434128310005" ID="ID_644247390" MODIFIED="1434128318561" TEXT="Knoten-ID"/>
+</node>
+<node CREATED="1434128074725" ID="ID_933994138" MODIFIED="1434128077625" TEXT="Diff-System">
+<node CREATED="1434128278990" ID="ID_106354755" MODIFIED="1434128283641" TEXT="Diff-Darstellung"/>
+<node CREATED="1434128267381" ID="ID_823706141" MODIFIED="1434128551925" TEXT="List-diff">
+<icon BUILTIN="go"/>
+</node>
+<node CREATED="1434128078638" ID="ID_1704749549" MODIFIED="1434128568744" TEXT="Tree-Diff">
+<icon BUILTIN="prepare"/>
+<node CREATED="1434128095838" ID="ID_419405890" MODIFIED="1434128561967" TEXT="Detector">
+<icon BUILTIN="stop"/>
+</node>
+<node CREATED="1434128092877" ID="ID_105246595" MODIFIED="1434128109517" TEXT="Applikator">
+<node CREATED="1434128115462" ID="ID_1299653797" MODIFIED="1434128119065" TEXT="Tree-Mutator"/>
+</node>
+<node CREATED="1434128083878" ID="ID_937754899" MODIFIED="1434128494612" TEXT="Format">
+<node CREATED="1434128153773" ID="ID_1289483934" MODIFIED="1434128577748" TEXT="Objekt-Repr&#xe4;s">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1434128243334" ID="ID_1828331212" MODIFIED="1434128248667" TEXT="Typ-Darstellung"/>
+<node CREATED="1434128239517" ID="ID_1886740948" MODIFIED="1434128250041" TEXT="Mapping"/>
+<node CREATED="1434128170381" ID="ID_976705384" MODIFIED="1434128380059" TEXT="GenNode">
+<linktarget COLOR="#ff0033" DESTINATION="ID_976705384" ENDARROW="Default" ENDINCLINATION="2;9;" ID="Arrow_ID_1285375088" SOURCE="ID_553361956" STARTARROW="Default" STARTINCLINATION="26;-13;"/>
+<node CREATED="1434128174030" ID="ID_1395250463" MODIFIED="1434128176521" TEXT="Variant"/>
+<node CREATED="1434128217645" ID="ID_1790054544" MODIFIED="1434128220257" TEXT="Monade"/>
+<node CREATED="1434128176918" ID="ID_863330674" MODIFIED="1434128195333" TEXT="Record">
+<node CREATED="1434128198957" ID="ID_1224215957" MODIFIED="1434128203817" TEXT="Konstuktor"/>
+<node CREATED="1434128179406" ID="ID_1833179523" MODIFIED="1434129153311" TEXT="Referez">
+<icon BUILTIN="pencil"/>
+<node CREATED="1434129158157" ID="ID_1777328498" MODIFIED="1434129165457" TEXT="sicher dereferenzierbar">
+<node CREATED="1434205928410" ID="ID_733269570" MODIFIED="1434205947253" TEXT="entweder zwangsweise gebunden"/>
+<node CREATED="1434205947841" ID="ID_871233558" MODIFIED="1434205955964" TEXT="oder NULL-Zustand mit Exception"/>
+<node CREATED="1434205957177" ID="ID_499991180" MODIFIED="1434205968740" TEXT="inherente Unsicherheit einer Referenz"/>
+</node>
+<node CREATED="1434129167805" ID="ID_819452470" MODIFIED="1434129251779" TEXT="stand-in">
+<arrowlink COLOR="#00ff33" DESTINATION="ID_654762061" ENDARROW="Default" ENDINCLINATION="457;0;" ID="Arrow_ID_724106052" STARTARROW="Default" STARTINCLINATION="457;0;"/>
+<icon BUILTIN="help"/>
+<node CREATED="1434129196709" ID="ID_1004519740" MODIFIED="1434150973939" TEXT="Subklasse von Rec">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      scheidet aus, wegen Wertsemantik
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1434129204149" ID="ID_1688475597" MODIFIED="1434129212537" TEXT="GenNode">
+<node CREATED="1434205661969" ID="ID_1484374626" MODIFIED="1434205705054">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      mit <i>speziellem </i>Ref-Typ
+    </p>
+    <p>
+      <font size="1">-- im DataCap</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node BACKGROUND_COLOR="#ffcccc" COLOR="#990033" CREATED="1434205598709" ID="ID_235720343" MODIFIED="1434205652458" TEXT="stand-in hei&#xdf;t...">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1434205739609" ID="ID_1185983904" MODIFIED="1434205778045" TEXT="kann anstelle eines Objektes treten"/>
+<node CREATED="1434205834506" ID="ID_1477654683" MODIFIED="1434205859229" TEXT="transparent f&#xfc;r den Aufrufer"/>
+<node CREATED="1434205862449" ID="ID_1736858324" MODIFIED="1434205879837" TEXT="Konsequenz: DataCap mu&#xdf; das verstehen"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1434128128869" ID="ID_244966341" MODIFIED="1434128131785" TEXT="Verben">
+<node CREATED="1434128134508" ID="ID_553361956" MODIFIED="1434128380058" TEXT="ID-Repr&#xe4;s">
+<arrowlink COLOR="#ff0033" DESTINATION="ID_976705384" ENDARROW="Default" ENDINCLINATION="2;9;" ID="Arrow_ID_1285375088" STARTARROW="Default" STARTINCLINATION="26;-13;"/>
+<node CREATED="1434128393429" ID="ID_1275202366" MODIFIED="1434128584214" TEXT="mu&#xdf; GenNode sein">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1434128412934" ID="ID_1319614474" MODIFIED="1434128461227" TEXT="Repr&#xe4;s entscheiden">
+<icon BUILTIN="pencil"/>
+<node CREATED="1434128438565" ID="ID_913220298" MODIFIED="1434128678545" TEXT="als ID erkennbar">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hei&#223;t: in der Diff-Verarbeitung wird dieser spezielle check verwendet
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1434128740117" ID="ID_1537979881" MODIFIED="1434128764209" TEXT="spezielles Baumuster"/>
+<node CREATED="1434128764893" ID="ID_1430586148" MODIFIED="1434128768689" TEXT="Gefahr von clashes"/>
+<node CREATED="1434128769325" ID="ID_866845827" MODIFIED="1434128777908" TEXT="entscheide">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1434128779661" ID="ID_1739097548" MODIFIED="1434128836241" TEXT="marker-ID + string-Payload"/>
+<node CREATED="1434128917125" ID="ID_392407967" MODIFIED="1434128927937" TEXT="&quot;fehlkonstruierte&quot; ID + pr&#xfc;f-Pr&#xe4;dikat"/>
+<node CREATED="1434128981381" ID="ID_101281763" MODIFIED="1434128988113" TEXT="spezielle Ref-Payload"/>
+</node>
+</node>
+<node CREATED="1434128446029" ID="ID_1779802587" MODIFIED="1434128654842" TEXT="hash-identisch">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hei&#223;t: wird direkt von standard-equality so behandelt
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1434128685597" ID="ID_690649535" MODIFIED="1434128705631">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      brauche speziellen Builder,
+    </p>
+    <p>
+      der das so fabriziert
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1434128706589" ID="ID_1001559218" MODIFIED="1434128728613">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      bekomme einen
+    </p>
+    <p>
+      &quot;ungenutzten&quot; DataCap
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1434128996949" ID="ID_654762061" MODIFIED="1434129237484" TEXT="k&#xf6;nnte zur Ref ausgebaut werden">
+<linktarget COLOR="#00ff33" DESTINATION="ID_654762061" ENDARROW="Default" ENDINCLINATION="457;0;" ID="Arrow_ID_724106052" SOURCE="ID_819452470" STARTARROW="Default" STARTINCLINATION="457;0;"/>
+</node>
+<node CREATED="1434130839653" ID="ID_60404225" MODIFIED="1434130915643">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Idee: <font color="#990033"><b>Ref-GenNode</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="idea"/>
+<node CREATED="1434130866693" ID="ID_1402852366" MODIFIED="1434130886826">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      als Ref erkennbar
+    </p>
+    <p>
+      <font size="1">(Pr&#228;dikat)</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1434130888581" ID="ID_369455584" MODIFIED="1434130912181">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hash-identische
+    </p>
+    <p>
+      Ziel-ID ableitbar
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1434206472689" ID="ID_561057428" MODIFIED="1434206482829" TEXT="Variant-Subklasse"/>
+<node CREATED="1434206483657" ID="ID_473311646" MODIFIED="1434206508445" TEXT="Cast auf Rec-Typ pr&#xfc;fen"/>
+<node CREATED="1434206509201" ID="ID_717222987" MODIFIED="1434206522117" TEXT="nur Ref-Fall zahlt Overhead"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1434129043245" ID="ID_1120430427" MODIFIED="1434129055076" TEXT="brauchen wir einen ref-Typ">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="help"/>
+<node CREATED="1434129063053" ID="ID_1242923371" MODIFIED="1434129082821">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Verarbeiten
+    </p>
+    <p>
+      von Teilb&#228;umen
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
