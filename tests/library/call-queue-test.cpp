87dc04f32435c435bf8575cfe764c5bf19420b81
Ichthyostega <prg@ichthyostega.de>
2017-08-05 18:44:25 +0200

    UI-Dispatch: verify consistency of argument data handling

--- tests/library/call-queue-test.cpp
+++ tests/library/call-queue-test.cpp
@@ -32,7 +32,6 @@
 #include "lib/call-queue.hpp"
 
 #include <functional>
-//#include <vector>
 #include <string>
 
 
@@ -46,8 +45,43 @@ namespace test{
   
   
   
-  namespace { // test data
-  }
+  namespace { // test fixture
+    
+    uint calc_sum = 0;
+    uint ctor_sum = 0;
+    uint dtor_sum = 0;
+    
+    template<uint i>
+    struct Dummy
+      {
+        uint val_;
+        
+        Dummy()
+          : val_(i)
+          {
+            ctor_sum += (val_+1);
+          }
+        
+       ~Dummy()
+          {
+            dtor_sum += val_;
+          }
+        
+        int
+        operator++()
+          {
+            return ++val_;
+          }
+      };
+    
+    template<uint i>
+    void
+    increment (Dummy<i>&& dummy)  //NOTE: dummy is consumed here
+    {
+      calc_sum += ++dummy;
+    }
+    
+  }//(End) test fixture
   
   
   
@@ -94,10 +128,41 @@ namespace test{
         }
       
       
+      /**
+       * @test consistency of queue data handling
+       *       - functors of various types can be mixed
+       *       - entries are moved in and out of the queue
+       *       - no copying whatsoever happens
+       *       - each entry gets invoked
+       *       - all entries are invoked in order
+       *       - enqueuing and dequeuing can be interspersed
+       *       - no memory leaks in bound argument data
+       */
       void
       verify_Consistency ()
         {
-          UNIMPLEMENTED("verify_Consistency");
+          calc_sum = 0;
+          ctor_sum = 0;
+          dtor_sum = 0;
+          
+          CallQueue queue;
+          queue.feed ([]() { increment(Dummy<0>{}); });     //NOTE: each lambda binds a different instantiation of the increment template
+          queue.feed ([]() { increment(Dummy<1>{}); });     //      and each invocation closes over an anonymous rvalue instance
+          queue.feed ([]() { increment(Dummy<2>{}); });
+          
+          queue.invoke();
+          queue.invoke();
+          queue.feed ([]() { increment(Dummy<3>{}); });
+          queue.feed ([]() { increment(Dummy<4>{}); });
+          
+          queue.invoke();
+          queue.invoke();
+          queue.invoke();
+          
+          uint expected = (5+1)*5/2;
+          CHECK (calc_sum = expected);
+          CHECK (ctor_sum = expected);
+          CHECK (dtor_sum = expected);
         }
       
       
