18d1e7a280956754a440ec56a6847f2191010ad0
Ichthyostega <prg@ichthyostega.de>
2017-10-02 18:11:21 +0200

    UI-Coordinates: polish test and consider next steps
    After completing the self-contained UICoord data elements,
    the next thing to consider might be how to resolve UI coordinates
    against an actual window topology. We need to define a suitable
    command-and-query interface in order to build and verify this
    intricate resolution process separated from the actual UI code.

---
+++ src/gui/interact/ui-coord-resolver.hpp
@@ -0,0 +1,106 @@
+/*
+  UI-COORD-RESOLVER.hpp  -  resolve UI coordinate spec against actual window topology
+
+  Copyright (C)         Lumiera.org
+    2017,               Hermann Vosseler <Ichthyostega@web.de>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+
+/** @file ui-coord-resolver.hpp
+ ** Evaluation of UI coordinates against a concrete window topology.
+ ** [UI-Coordinates](\ref UICoord) allow to describe and locate an interface component within the
+ ** Lumiera GUI through a topological access path. As such these coordinate specifications are abstract,
+ ** and need to be related, attached or resolved against the actual configuration of widgets in the UI.
+ ** Through this relation it becomes possible to pose meaningful queries over these coordinates, or to
+ ** build, rebuild and remould a coordinate specification.
+ ** 
+ ** We need to avoid tainting with the intrinsics of the actual UI toolkit though -- which indicates
+ ** the UICoordResolver should be designed as an abstract intermediary, built on top of a command and
+ ** query interface, provided by the \ref Navigator and backed by the actual UI configuration.
+ ** 
+ ** @todo WIP 10/2017 first draft ////////////////////////////////////////////////////////////////////////////TICKET #1106 generic UI coordinate system
+ ** 
+ ** @see UICoordResolver_test
+ ** @see UICoord_test
+ ** @see navigator.hpp
+ ** @see view-locator.hpp
+ */
+
+
+#ifndef GUI_INTERACT_UI_COORD_RESOLVER_H
+#define GUI_INTERACT_UI_COORD_RESOLVER_H
+
+#include "lib/error.hpp"
+//#include "lib/symbol.hpp"
+#include "gui/interact/ui-coord.hpp"
+//#include "lib/util.hpp"
+
+//#include <boost/noncopyable.hpp>
+//#include <string>
+//#include <vector>
+//#include <utility>
+//#include <memory>
+
+
+namespace gui {
+namespace interact {
+  
+  namespace error = lumiera::error;
+  
+//  using std::unique_ptr;
+//  using std::string;
+//  using lib::Literal;
+//  using lib::Symbol;
+//  using util::unConst;
+//  using util::isnil;
+//  using util::min;
+  
+
+  
+  
+  /**
+   * Query and mutate UICoord specifications in relation to actual UI topology.
+   * 
+   * @todo initial draft as of 10/2017
+   */
+  class UICoordResolver
+    {
+      
+    public:
+      
+      
+      
+      /* === named component access === */
+      
+      /** */
+      
+      
+      
+      /* === query functions === */
+      
+      /**
+       */
+      
+      
+    private:
+    };
+  
+  
+  
+}}// namespace gui::interact
+#endif /*GUI_INTERACT_UI_COORD_RESOLVER_H*/
--- tests/gui/interact/ui-coord-resolver-test.cpp
+++ tests/gui/interact/ui-coord-resolver-test.cpp
@@ -1,5 +1,5 @@
 /*
-  UICoord(Test)  -  properties of topological UI coordinate specifications
+  UICoordResolver(Test)  -  resolve UI coordinates against actual topology
 
   Copyright (C)         Lumiera.org
     2017,               Hermann Vosseler <Ichthyostega@web.de>
@@ -20,14 +20,15 @@
 
 * *****************************************************/
 
-/** @file ui-coord-test.cpp
- ** unit test \ref UICoord_test
+/** @file ui-coord-resolver-test.cpp
+ ** unit test \ref UICoordResolver_test
  */
 
 
 #include "lib/test/run.hpp"
 #include "lib/test/test-helper.hpp"
 #include "gui/interact/ui-coord.hpp"
+#include "gui/interact/ui-coord-resolver.hpp"
 #include "lib/format-cout.hpp"/////////////////////////TODO RLY?
 #include "lib/format-util.hpp"
 //#include "lib/idi/entry-id.hpp"
@@ -61,21 +62,14 @@ namespace test {
   
   
   /******************************************************************************//**
-   * @test verify the basic properties of topological UI coordinate specifications.
-   *       - created as path-like sequence of components
-   *       - provides a builder API for definition and mutation
-   *       - Normalisation and handling of missing parts
-   *       - access to UI coordinate components
-   *       - string representation
-   *       - comparisons
+   * @test verify query and mutation of UICoord in relation to actual UI topology.
    * 
-   * @see ui-coord.hpp
-   * @see path-array.hpp
-   * @see PathArray_test
-   * @see UICoordResolver_test
-   * @see ViewSpecDSL_test
+   * @see UICoordResolver
+   * @see navigator.hpp
+   * @see ViewLocator
+   * @see UICoord_test
    */
-  class UICoord_test : public Test
+  class UICoordResolver_test : public Test
     {
       
       virtual void
@@ -104,19 +98,19 @@ namespace test {
           CHECK (not isnil (uic));
           CHECK (8 == uic.size());
           // path is iterable
-          CHECK ("Γ-Δ-Θ-Ξ-Σ-Ψ-Φ-Ω" == join(uic,"-"));
+          CHECK ("ΓΔΘΞΣΨΦΩ" == join(uic,""));
           
           // indexed access
-          CHECK ("Γ" == uic[UIC_WINDOW]);    // window spec to anchor the path
-          CHECK ("Δ" == uic[UIC_PERSP]);     // the perspective used within that window
-          CHECK ("Θ" == uic[UIC_PANEL]);     // the docking panel within the window
-          CHECK ("Ξ" == uic[UIC_VIEW]);      // the view residing in the docking panel
-          CHECK ("Σ" == uic[UIC_TAB]);       // the tab or element group within the view
-          CHECK ("Ψ" == uic[UIC_PATH]);      // a path sequence...
-          CHECK ("Φ" == uic[UIC_PATH+1]);    // ...descending through local widgets
+          CHECK ("Γ" == uic[UIC_WINDOW]);
+          CHECK ("Δ" == uic[UIC_PERSP]);
+          CHECK ("Θ" == uic[UIC_PANEL]);
+          CHECK ("Ξ" == uic[UIC_VIEW]);
+          CHECK ("Σ" == uic[UIC_TAB]);
+          CHECK ("Ψ" == uic[UIC_PATH]);
+          CHECK ("Φ" == uic[UIC_PATH+1]);
           CHECK ("Ω" == uic[UIC_PATH+2]);
           
-          // iteration of complete path matches index order
+          // iteration matches index order
           uint i=0;
           for (UICoord::iterator ii = uic.begin(); ii; ++ii, ++i)
             CHECK (uic[i] == *ii);
@@ -126,8 +120,8 @@ namespace test {
           
           // string representation
           CHECK ("UI:Γ[Δ]-Θ.Ξ.Σ/Ψ/Φ/Ω" == string(uic));
-          CHECK (   "Γ[Δ]-Θ.Ξ.Σ"       == uic.getComp());
-          CHECK (              "Ψ/Φ/Ω" == uic.getPath());
+          CHECK ("Γ[Δ]-Θ.Ξ.Σ" == uic.getComp());
+          CHECK ("Ψ/Φ/Ω" == uic.getPath());
           
           // new value can be assigned, but not altered in place
           uic = UICoord{nullptr,nullptr,"Θ",nullptr,"Σ",nullptr,"Φ"};
@@ -150,8 +144,8 @@ namespace test {
       verify_builder()
         {
           UICoord uic1 = UICoord::window("window");
-          UICoord uic2 = uic1.view("view");     // Note: does not alter uic1
-          CHECK ("UI:window"           == string(uic1));
+          UICoord uic2 = uic1.view("view");
+          CHECK ("UI:window" == string(uic1));
           CHECK ("UI:window[*]-*.view" == string(uic2));
           CHECK (1 == uic1.size());
           CHECK (4 == uic2.size());
@@ -175,7 +169,6 @@ namespace test {
           VERIFY_ERROR (INDEX_BOUNDS, uic1[UIC_PERSP]);
           VERIFY_ERROR (INDEX_BOUNDS, uic2[UIC_TAB]);
           
-          // partial (incomplete) coordinate spec
           UICoord uic3 = UICoord().view("view");
           CHECK (4 == uic3.size());
           CHECK ("UI:?.view" == string(uic3));
@@ -184,9 +177,9 @@ namespace test {
           CHECK (""     == uic3.getPanel());
           CHECK ("view" == uic3.getView());
           
-          UICoord uic4 = uic3.persp("perspective");
+          UICoord uic4 = uic3.persp("persp");
           CHECK (4 == uic4.size());
-          CHECK ("UI:?[perspective]-*.view" == string(uic4));
+          CHECK ("UI:?[persp]-*.view" == string(uic4));
 
           uic4 = uic3.append("tab");
           CHECK (5 == uic4.size());
@@ -275,11 +268,6 @@ namespace test {
           uic = uic.view(nullptr);
           CHECK ("UI:?-panel.*.*/α/β/γ/δ/ε/λ/ον" == string(uic));
           CHECK ("?-panel.*.*" == uic.getComp());
-          CHECK ("α/β/γ/δ/ε/λ/ον" == uic.getPath());
-          
-          uic = uic.tab(8);
-          CHECK ("UI:?-panel.*.#8/α/β/γ/δ/ε/λ/ον" == string(uic));
-          CHECK ("?-panel.*. " == uic.getComp());
           CHECK ("α/β/γ/δ/ε/λ/ον" == uic.getPath());
           
           uic = uic.tab(" ");
@@ -402,7 +390,7 @@ namespace test {
   
   
   /** Register this test class... */
-  LAUNCHER (UICoord_test, "unit gui");
+  LAUNCHER (UICoordResolver_test, "unit gui");
   
   
 }}} // namespace gui::interact::test
