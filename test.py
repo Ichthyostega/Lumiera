35e74d755d247754ae5c487fc8c81b1a413dc3bb
Russel Winder <russel@winder.org.uk>
2020-06-02 19:26:31 +0100

    sconsDoxy: Import the final state of the Mercurial repository without the history.

--- test.py
+++ test.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python
+# -*- coding=utf-8; -*-
+
+# Tests for SCons Doxygen builder.
 #
-# tests for scons doxygen builder
+# Copyright © 2013  Richard van der Hoff
+# Copyright © 2013  Russel Winder
 #
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import unittest
 import os
 import sys
 from doxygen import DoxyfileParse
 
+
 class TestParser(unittest.TestCase):
-    test_config_dir = os.path.join(os.path.dirname(__file__),'test_config')
+    test_config_dir = os.path.join(os.path.dirname(__file__), 'test_config')
 
     def test_simple_parse(self):
-        text="""
+        text = """
 # comment
 INPUT  = test.h
 """
@@ -20,20 +37,19 @@ INPUT  = test.h
         self.assertEqual(["test.h"], result["INPUT"])
 
     def test_parse_tag_on_first_line(self):
-        text="""INPUT=."""
+        text = """INPUT=."""
         result = DoxyfileParse(text, self.test_config_dir)
         self.assertEqual(["."], result["INPUT"])
 
     def test_include_tag(self):
-        text="""@INCLUDE=include_test.cfg"""
+        text = """@INCLUDE=include_test.cfg"""
         result = DoxyfileParse(text, self.test_config_dir)
         self.assertEqual(["abc"], result["INPUT"])
-        self.assertEqual([os.path.join(self.test_config_dir,
-                                       "include_test.cfg")],
+        self.assertEqual([os.path.join(self.test_config_dir, "include_test.cfg")],
                          result["@INCLUDE"])
 
     def test_recursive_include_tag(self):
-        text="""@INCLUDE=recursive_include_test.cfg"""
+        text = """@INCLUDE=recursive_include_test.cfg"""
         self.assertRaises(Exception, DoxyfileParse, text, self.test_config_dir)
 
 
