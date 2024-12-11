22f4b9dd7e52adacc802610a99e251335c74502c
Ichthyostega <prg@ichthyostega.de>
2024-12-11 02:36:17 +0100

    Invocation: implement the chaining and linking functionality
    This basically solves this implementation challenge:
    It was possible to construct a ''compile-time type-safe'' overlay,
    while using force-casts ''without any metadata'' at runtime.
    
    Obviously this is a dangerous setup, but ''should be resonably safe'' when used within the defined scheme...

--- tests/library/hetero-data-test.cpp
+++ tests/library/hetero-data-test.cpp
@@ -20,19 +20,18 @@
 #include "lib/hetero-data.hpp"
 #include "lib/meta/trait.hpp"
 #include "lib/test/diagnostic-output.hpp"/////////////////TODO
+#include "lib/util.hpp"
 
+#include <string>
 
 
 namespace lib {
 namespace test{
   
+  using std::string;
   using meta::is_Subclass;
+  using util::isSameObject;
   
-  namespace { // probe victims
-    
-  }//(End) test data
-  
-  #define TYPE(_EXPR_) showType<decltype(_EXPR_)>()
   
   
   
@@ -54,11 +53,13 @@ namespace test{
 //          checksum = 0;
           
           verify_FrontBlock();
+          verify_ChainBlock();
         }
       
       
+      /** @test build a free standing data tuple block to start a chain */
       void
-      verify_FrontBlock ()
+      verify_FrontBlock()
         {
           using Block1 = HeteroData<uint,double>;
           CHECK ((is_Subclass<Block1::NewFrame, std::tuple<uint,double>>()));
@@ -73,6 +74,41 @@ namespace test{
           CHECK (0.0 == b2.get<1>());
           b2.get<1>() = 3.14;
           CHECK (3.14 == b2.get<1>());
+        }
+      
+      
+      /** @test construct a follow-up data tuple block and hook it into the chain */
+      void
+      verify_ChainBlock()
+        {
+          using Block1 = HeteroData<uint>;
+          CHECK ((is_Subclass<Block1::NewFrame, std::tuple<uint>>()));
+          
+          using Constructor = Block1::Chain<double,string>;
+          using Block2 = Constructor::NewFrame;
+          CHECK ((is_Subclass<Block2, std::tuple<double, string>>()));
+          
+          auto b1 = Block1::build (41);
+          auto b2 = Constructor::build (1.61, "Φ");
+          b2.linkInto(b1);
+          
+          auto& chain2 = reinterpret_cast<Constructor::ChainType&> (b1);
+          CHECK (b1.size()     == 1);
+          CHECK (chain2.size() == 3);
+          
+          CHECK (41   == chain2.get<0>());
+          CHECK (1.61 == chain2.get<1>());
+          CHECK ("Φ"  == chain2.get<2>());
+          
+          chain2.get<0>()++;
+          chain2.get<1>() = (1 + sqrt(5)) / 2;
+          
+          CHECK (b1.get<0>()      == 42);
+          CHECK (chain2.get<0>()  == 42);
+          CHECK (std::get<0> (b2) == "1.618034"_expect);
+          
+          CHECK (isSameObject (chain2.get<0>() ,b1.get<0>()));
+          CHECK (isSameObject (chain2.get<2>() ,std::get<1>(b2)));
         }
     };
   
