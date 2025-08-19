7bbb21a8adeb16d57b5141acfd6a4004e4e029ee
Ichthyostega <prg@ichthyostega.de>
2025-08-19 15:15:36 +0200

    DOC: minutes of FrOSCon meeting
    Topic: proposals for Lumiera Workflow
    Present:
    - Wouter Verweijlen
    - Benny Lyons
    - Hermann Voßeler
    
    Note: This commit creates a new subsection
          for the discussion related to Wouter's »Lumiera Workflow Proposals«....

---
+++ wiki/Workflow_Verwijlen.mm
@@ -0,0 +1,149 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#e3ab8e" COLOR="#63384c" CREATED="1755554646762" ID="ID_1535128529" MODIFIED="1755554741922" TEXT="Wouter">
+<font NAME="SansSerif" SIZE="19"/>
+<node CREATED="1755555124099" ID="ID_1161663096" MODIFIED="1755555131868" POSITION="right" TEXT="zeitlicher Ablauf">
+<node CREATED="1755555133354" ID="ID_830170698" MODIFIED="1755555167568" TEXT="hatte ich vor &gt; 10 Jahren bei der LAC Utrecht kennengelernt"/>
+<node CREATED="1755555168135" ID="ID_106572724" MODIFIED="1755555192830" TEXT="war Fr&#xfc;hjahr 2025 wieder in einem Entwicklertreffen auf IRC aufgetaucht"/>
+<node CREATED="1755555193178" ID="ID_341875247" MODIFIED="1755555231518" TEXT="schreibt ein Dokument &#xbb;Lumiera Workflow Proposals&#xab;">
+<node CREATED="1755555238933" ID="ID_776839044" MODIFIED="1755555245419" TEXT="baut das schrittweise aus"/>
+<node CREATED="1755555246177" ID="ID_1744077729" MODIFIED="1755555276905" TEXT="Anfang: Arranging and Grouping in the Timeline"/>
+<node CREATED="1755555296893" ID="ID_632124040" MODIFIED="1755555300024" TEXT="..."/>
+<node CREATED="1755555300806" ID="ID_1430647099" MODIFIED="1755555311311" TEXT="Juli: &#xfc;berarbeitete Version"/>
+</node>
+<node CREATED="1755555313311" ID="ID_293738515" MODIFIED="1755555326302" TEXT="kommt zur FrOSCon 25 um uns zu treffen">
+<node CREATED="1755556763369" ID="ID_1392720362" MODIFIED="1755556767239" TEXT="Diskussion">
+<node CREATED="1755556772829" FOLDED="true" ID="ID_221527579" MODIFIED="1755558351493" TEXT="Modes / Tools">
+<node CREATED="1755556805699" ID="ID_1983034292" MODIFIED="1755556820358" TEXT="ich greife eine Idee aus Wouter&apos;s letzter Version auf">
+<node CREATED="1755556828897" ID="ID_1292446432" MODIFIED="1755556852232" TEXT="er schl&#xe4;gt vor: Art des Trim kann nach Wahl des Trim-Tool noch ge&#xe4;ndert werden"/>
+<node CREATED="1755556852984" ID="ID_1732897512" MODIFIED="1755556874830" TEXT="Tool-Overlay vorgeschlagen"/>
+<node CREATED="1755556875503" ID="ID_270727640" MODIFIED="1755556886969" TEXT="ich verbinde das mit Blender"/>
+<node CREATED="1755556887708" ID="ID_1263044258" MODIFIED="1755556905380" TEXT="mein Begriff: &#xbb;postfix qualification&#xab;"/>
+</node>
+<node CREATED="1755556920393" ID="ID_1329266614" MODIFIED="1755556947710" TEXT="ich suche seit langer Zeit nach einem radikal-einheitlichen GUI-Konzept">
+<node CREATED="1755556949740" ID="ID_271446237" MODIFIED="1755556967520" TEXT="ich m&#xf6;chte &#xbb;Gestures&#xab; in mehreren &#xbb;Control-Systems&#xab; implementieren"/>
+<node CREATED="1755556967952" ID="ID_209453569" MODIFIED="1755556997719" TEXT="ich m&#xf6;chte eine neue Zwischenschicht im GUI schaffen"/>
+</node>
+<node CREATED="1755557005509" ID="ID_312808136" MODIFIED="1755557029538" TEXT="mein Vorschlag">
+<node CREATED="1755557030890" ID="ID_1600977397" MODIFIED="1755557037640" TEXT="wir haben stets einen Tool-Mode"/>
+<node CREATED="1755557038147" ID="ID_1685437604" MODIFIED="1755557061336" TEXT="Modi sind hierarchisch und vernetzt"/>
+<node CREATED="1755557062209" ID="ID_1199958546" MODIFIED="1755557072514" TEXT="Top / Default ist ein Navigation-Mode"/>
+</node>
+<node CREATED="1755557136983" ID="ID_1143798808" MODIFIED="1755557141797" TEXT="Diskussionspunkte">
+<node CREATED="1755557146907" ID="ID_1777408367" MODIFIED="1755557157747" TEXT="erscheint Wouter grunds&#xe4;tzlich plausibel"/>
+<node CREATED="1755557167504" ID="ID_1452128372" MODIFIED="1755558324554" TEXT="brauchen wir einen move-Mode?">
+<arrowlink COLOR="#a9b4c1" DESTINATION="ID_288360112" ENDARROW="Default" ENDINCLINATION="-273;24;" ID="Arrow_ID_1301604055" STARTARROW="None" STARTINCLINATION="-550;50;"/>
+</node>
+<node CREATED="1755557178881" ID="ID_44797951" MODIFIED="1755557201468" TEXT="kann man Modes mit einem einzigen Schritt aktivieren?">
+<node CREATED="1755557217629" ID="ID_1011982875" MODIFIED="1755557223701" TEXT="oder geht man in eine Meta-Ebene"/>
+<node CREATED="1755557224134" ID="ID_1710277662" MODIFIED="1755557231046" TEXT="und w&#xe4;hlt und aktiviert?"/>
+</node>
+</node>
+<node CREATED="1755557522468" ID="ID_362435549" MODIFIED="1755557524357" TEXT="offen">
+<node CREATED="1755557526532" ID="ID_1178754034" MODIFIED="1755557541412" TEXT="l&#xe4;&#xdf;t sich das konsistent durchziehen?"/>
+<node CREATED="1755557541925" ID="ID_1173750228" MODIFIED="1755557562298" TEXT="funktioniert es auch mit Effect-Settings und Mixern?"/>
+</node>
+</node>
+<node CREATED="1755558194157" ID="ID_1955084053" MODIFIED="1755558219871" TEXT="I18N -- right-to-left-Languages...?"/>
+<node CREATED="1755558224232" ID="ID_901437535" MODIFIED="1755558227585" TEXT="Placements">
+<node CREATED="1755558233284" ID="ID_879325256" MODIFIED="1755558243988" TEXT="Danger to confuse and overload the user"/>
+<node CREATED="1755558386563" ID="ID_1013996362" MODIFIED="1755558406406" TEXT="Solution: introduce Placement-Prototypes (very few)"/>
+<node CREATED="1755558414107" ID="ID_804074577" MODIFIED="1755558418411" TEXT="need Diagnostics?"/>
+</node>
+<node CREATED="1755559039132" ID="ID_1600869743" MODIFIED="1755559041171" TEXT="Routing">
+<node CREATED="1755559044499" ID="ID_329815119" MODIFIED="1755559065242" TEXT="ich erkl&#xe4;re wie Routing auf Placements + Rules + Tags aufbaut"/>
+<node CREATED="1755559078313" ID="ID_805446259" MODIFIED="1755559086483" TEXT="Tags k&#xf6;nnen Subgruppen erzeugen"/>
+<node CREATED="1755559118276" ID="ID_1105838710" MODIFIED="1755559130289" TEXT="Problem der Layering-Order"/>
+</node>
+<node CREATED="1755559304126" ID="ID_1393271714" MODIFIED="1755559311112" TEXT="Grouping devices">
+<node CREATED="1755559313671" ID="ID_811242477" MODIFIED="1755559332439" TEXT="Wouter hat verstanden, da&#xdf; ich ungern mehrere Konzepte einf&#xfc;hre"/>
+<node CREATED="1755559332916" ID="ID_82928485" MODIFIED="1755559383224" TEXT="ich habe verstanden, warum Wouter ein offenes Grouping-Device m&#xf6;chte">
+<node CREATED="1755559385369" ID="ID_310970531" MODIFIED="1755559409569" TEXT="er f&#xfc;rchtet, da&#xdf; durch Einfalten/&#xd6;ffenen/Schlie&#xdf;en die Struktur un&#xfc;bersichtlich wird"/>
+<node CREATED="1755559421275" ID="ID_804411599" MODIFIED="1755559443502" TEXT="er m&#xf6;chte trotz Gruppierung die Clips direkt bearbeiten k&#xf6;nnen"/>
+</node>
+<node CREATED="1755559455854" ID="ID_5612422" MODIFIED="1755559472564" TEXT="ich w&#xe4;re deshalb bereit, eine placement-basierte Gruppe einzuf&#xfc;hren"/>
+<node CREATED="1755559473231" ID="ID_1431795441" MODIFIED="1755559499337" TEXT="aber ich schlage vor, die top-level-Segments als nested Sequences zu implementieren"/>
+<node CREATED="1755559519445" ID="ID_51731980" MODIFIED="1755559540950" TEXT="das erfordert jedoch ein abgestuftes Expanding">
+<node CREATED="1755559549728" ID="ID_231845718" MODIFIED="1755559561547" TEXT="brauche eine Zwischenstufe die vertikal Platz spart"/>
+<node CREATED="1755559561990" ID="ID_1124062159" MODIFIED="1755559585789" TEXT="aber dennoch die einzelnen Clips zug&#xe4;nglich  macht"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1755555354222" ID="ID_251001171" MODIFIED="1755555370422" POSITION="right" TEXT="&#xbb;Workflow Proposals&#xab;">
+<node CREATED="1755555375598" ID="ID_52787515" MODIFIED="1755555390542" TEXT="Ausgangspunkt: Arranging and Grouping"/>
+<node CREATED="1755555391813" ID="ID_1417799013" MODIFIED="1755555398129" TEXT="Einleitung: Personas"/>
+<node CREATED="1755555399076" ID="ID_431801249" MODIFIED="1755555416692" TEXT="Ausarbeitung">
+<node CREATED="1755555422659" ID="ID_1908160151" MODIFIED="1755555428905" TEXT="Working in the Timeline"/>
+</node>
+</node>
+<node CREATED="1755556060078" ID="ID_1432823078" MODIFIED="1755556068691" POSITION="right" TEXT="GUI Concept">
+<node CREATED="1755556073409" ID="ID_1321367154" MODIFIED="1755556083367" TEXT="erst mal ein Ziel">
+<node CREATED="1755556084594" ID="ID_870786719" MODIFIED="1755556091913" TEXT="von Wouter vorgeschlagen"/>
+<node CREATED="1755556092271" ID="ID_819223339" MODIFIED="1755556098507" TEXT="von Benny aufgegriffen"/>
+<node CREATED="1755556108907" ID="ID_676057805" MODIFIED="1755556123397">
+<richcontent TYPE="NODE"><html>
+  <head/>
+  <body>
+    <p>
+      ich sehe das als <i>G&#252;nstige Gelegenheit</i>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1755556128208" ID="ID_441917492" MODIFIED="1755556148810" TEXT="und bin deshalb gerene bereit, es jetzt auszuarbeiten"/>
+<node CREATED="1755556161801" ID="ID_1578183937" MODIFIED="1755556198680" TEXT="obwohl es nicht unmittelbar auf der Agenda steht">
+<richcontent TYPE="NOTE"><html>
+  <head/>
+  <body>
+    <p>
+      derzeit (2025) konzentriere ich die Arbeit auf die Integration der Kern-Applikation
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1755555823000" ID="ID_727948180" MODIFIED="1755555828691" POSITION="left" TEXT="Konzepte"/>
+<node CREATED="1755555829321" ID="ID_1938296529" MODIFIED="1755555835151" POSITION="left" TEXT="Diskussionen">
+<node CREATED="1755556578048" ID="ID_1963238475" MODIFIED="1755556589373" TEXT="Modes / Tools">
+<node CREATED="1755557599706" ID="ID_1052829600" MODIFIED="1755557603831" TEXT="universell?"/>
+<node CREATED="1755557604283" ID="ID_325794280" MODIFIED="1755557612568" TEXT="Move-Mode als Default?"/>
+<node CREATED="1755557613339" ID="ID_1555290019" MODIFIED="1755557620789" TEXT="Verschieben von Clips?"/>
+</node>
+<node CREATED="1755556603900" ID="ID_1246814540" MODIFIED="1755556609363" TEXT="Placements">
+<node CREATED="1755557908949" ID="ID_57692141" MODIFIED="1755557927536" TEXT="Limitatons of &#xbb;magnetic timeline&#xab;">
+<node CREATED="1755557932486" ID="ID_1844647796" MODIFIED="1755557935941" TEXT="mouse-centric"/>
+<node CREATED="1755557937684" ID="ID_734257727" MODIFIED="1755557949577" TEXT="only one Backbone"/>
+</node>
+<node CREATED="1755558029943" ID="ID_680705693" MODIFIED="1755558044006" TEXT="Danger of confusion"/>
+</node>
+<node CREATED="1755556610066" ID="ID_547966841" MODIFIED="1755556616938" TEXT="Grouping">
+<node CREATED="1755559739307" ID="ID_826369344" MODIFIED="1755559772670" TEXT="group members must be accessible"/>
+<node CREATED="1755560023780" ID="ID_753928271" MODIFIED="1755560048837" TEXT="Conflict: Layering Order vs. logical Grouping"/>
+<node CREATED="1755560075724" ID="ID_1551439558" MODIFIED="1755560089863" TEXT="Tracks solten leicht umzuordnen sein"/>
+<node CREATED="1755560058976" ID="ID_665070443" MODIFIED="1755560074138" TEXT="offen: wie kann man dann das Layering steuern"/>
+</node>
+</node>
+<node CREATED="1755555849864" ID="ID_679699435" MODIFIED="1755555853351" POSITION="left" TEXT="Beschl&#xfc;sse">
+<node CREATED="1755558439952" ID="ID_1246766903" MODIFIED="1755558441512" TEXT="Modes">
+<node CREATED="1755557755648" ID="ID_1848383743" MODIFIED="1755557790892" TEXT="handle Navigation as default-Mode"/>
+<node CREATED="1755557742539" ID="ID_689267684" MODIFIED="1755557754429" TEXT="mode-sensitive Tool-palette"/>
+<node CREATED="1755557814561" ID="ID_375965662" MODIFIED="1755557824931" TEXT="no special Move-Mode"/>
+<node CREATED="1755557796012" ID="ID_288360112" MODIFIED="1755558324555" TEXT="add move-lock to the Navigation Mode">
+<linktarget COLOR="#a9b4c1" DESTINATION="ID_288360112" ENDARROW="Default" ENDINCLINATION="-273;24;" ID="Arrow_ID_1301604055" SOURCE="ID_1452128372" STARTARROW="None" STARTINCLINATION="-550;50;"/>
+</node>
+</node>
+<node CREATED="1755559009639" ID="ID_1230328586" MODIFIED="1755559014439" TEXT="Placements">
+<node CREATED="1755559016769" ID="ID_1850978375" MODIFIED="1755559024924" TEXT="need Prototypes"/>
+<node CREATED="1755559679690" ID="ID_977787374" MODIFIED="1755559691735" TEXT="need diagnostics"/>
+</node>
+</node>
+<node CREATED="1755556219662" ID="ID_254042943" MODIFIED="1755556223566" POSITION="left" TEXT="Ver&#xf6;ffentlichung">
+<node COLOR="#44169d" CREATED="1755556232186" ID="ID_807805995" MODIFIED="1755556248453" TEXT="&#xbb;Workflow Proposal Verwijlen&#xab;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
